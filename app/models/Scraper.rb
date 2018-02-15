dd6cae1ca30d23c59a35efd9afae9ab017e86157
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2018-02-15 15:18:43 -0500

    made class

--- app/models/Scraper.rb
+++ app/models/Scraper.rb
@@ -5,20 +5,25 @@ require 'Pry'
 require 'JSON'
 require 'csv'
 
+class APTScraper
+  def initialize(link)
+    @page = HTTParty.get(link)
+    @parsed_page = Nokogiri::HTML(@page)
+  end
 
-page = HTTParty.get('https://miami.craigslist.org/search/apa?min_bedrooms=3&max_bedrooms=2&availabilityMode=0&sale_date=all+dates')
-parsed_page = Nokogiri::HTML(page)
-
-# for names
-names = parsed_page.css('.result-row').css('.result-info').css('.result-title').css('.hdrlnk').map{|x| x.text}
-# for links
-links = parsed_page.css('.content').css('.rows').css('.result-row').css('.result-info a').map{|l| l['href'] if l['href'].length>1}.reject{|w| w == nil}
-
-listings = Hash.new
-names.length.times do |i|
-  listings[i+1] = {'name' => names[i], 'link' => links[i]}
+  def get_listingsCSV
+    names = @parsed_page.css('.result-row').css('.result-info').css('.result-title').css('.hdrlnk').map{|x| x.text}
+    links = @parsed_page.css('.content').css('.rows').css('.result-row').css('.result-info a').map{|l| l['href'] if l['href'].length>1}.reject{|w| w == nil}
+      listings = Hash.new
+      names.length.times do |i|
+        listings[i+1] = {'name' => names[i], 'link' => links[i]}
+      end
+      CSV.open("listings.csv", "wb") {|csv| listings.to_a.each {|apt| csv << apt} }
+  end
 end
 
-CSV.open("listings.csv", "wb") {|csv| listings.to_a.each {|apt| csv << apt} }
+myLink = 'https://miami.craigslist.org/search/apa?min_bedrooms=3&max_bedrooms=2&availabilityMode=0&sale_date=all+dates'
+s = APTScraper.new(myLink)
+s.get_listingsCSV
 
 # Pry.start(binding)
